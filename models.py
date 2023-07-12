1363de16ce766b58a8493f72a22ebd8fc9598e88
Grant Ramsay <seapagan@gmail.com>
2023-07-12 17:56:31 +0100

    define models for Product and Manufacturer

--- models.py
+++ models.py
@@ -1,18 +1,41 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional
+
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from db import Model
 
 
 class Product(Model):
-    __tablename__ = "products"
+    __tablename__ = 'products'
 
-    id: Mapped[int] = mapped_column(String, primary_key=True)
-    name: Mapped[str] = mapped_column(String(64))
-    manufacturer: Mapped[str] = mapped_column(String(64))
-    year: Mapped[int]
-    country: Mapped[str] = mapped_column(String(32))
-    cpu: Mapped[str] = mapped_column(String(32))
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
+    manufacturer_id: Mapped[int] = mapped_column(
+        ForeignKey('manufacturers.id'),
+        index=True
+    )
+    year: Mapped[int] = mapped_column(index=True)
+    country: Mapped[Optional[str]] = mapped_column(String(32))
+    cpu: Mapped[Optional[str]] = mapped_column(String(32))
+
+    manufacturer: Mapped['Manufacturer'] = relationship(
+        back_populates='products'
+    )
 
     def __repr__(self):
-        return f'Product({self.id}), "{self.name}"'
+        return f'Product({self.id}, "{self.name}")'
+
+
+class Manufacturer(Model):
+    __tablename__ = 'manufacturers'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
+
+    products: Mapped[list[Product]] = relationship(
+        back_populates='manufacturer'
+    )
+
+    def __repr__(self):
+        return f'Manufacturer({self.id}, "{self.name}")'
