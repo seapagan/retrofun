5a298d52e6481adf8b923f55f6b790c3b9cd0e33
Grant Ramsay <seapagan@gmail.com>
2023-07-13 20:26:53 +0100

    update import to deal with Country table
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- import_products.py
+++ import_products.py
@@ -2,7 +2,7 @@
 import csv
 
 from db import Model, Session, engine
-from models import Manufacturer, Product
+from models import Country, Manufacturer, Product
 
 
 def main():
@@ -15,11 +15,14 @@ def main():
             with open("products.csv") as f:
                 reader = csv.DictReader(f)
                 all_manufacturers = {}
+                all_countries = {}
 
                 for row in reader:
                     row["year"] = int(row["year"])
 
                     manufacturer = row.pop("manufacturer")
+                    countries = row.pop("country").split("/")
+
                     p = Product(**row)
 
                     if manufacturer not in all_manufacturers:
@@ -27,6 +30,13 @@ def main():
                         session.add(m)
                         all_manufacturers[manufacturer] = m
                     all_manufacturers[manufacturer].products.append(p)
+
+                    for country in countries:
+                        if country not in all_countries:
+                            c = Country(name=country)
+                            session.add(c)
+                            all_countries[country] = c
+                        all_countries[country].products.append(p)
 
 
 if __name__ == "__main__":
